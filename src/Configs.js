3c6c2937683c3e60c375be767aad145d8ac697f2
nhatduy <nhatduy1tn@gmail.com>
2017-12-25 02:28:03 +0700

    authentication

---
+++ src/Configs.js
@@ -0,0 +1,8 @@
+module.exports = {
+    AUTH0: {
+        DOMAIN: "duyho.auth0.com",
+        CLIENT_ID: "SljoMQlqA5GdS5w4Cj19uY6tF3Q46RLk",
+        REDIRECT_URL: "http://localhost:3005/callback",
+        AUDIENCE: "https://duyho.kcoin.com"
+    }
+};
---
+++ src/Login.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {login} from './Services/AuthService';
+
+class Login extends Component {
+    login(){
+        login();
+
+    }
+    constructor() {
+        super()
+    }
+
+    render() {
+        return(
+            <button onClick={() => this.login()}>Login</button>
+        )
+    }
+}
+
+export default Login;
