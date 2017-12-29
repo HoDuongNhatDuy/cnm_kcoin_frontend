508455f7ef442ee3a229b88f660d3357cd67db51
Jake <forte2610@hotmail.com>
2017-12-29 17:47:53 +0700

    Fixed incorrectly placed css in Balance

--- src/balance/Balance.js
+++ src/balance/Balance.js
@@ -7,12 +7,12 @@ class Balance extends Component {
     render() {
         return (
             <div className="Balance">
-                <div className="col-sm-offset-1 col-sm-5 text-center pointer">
+                <div className="col-sm-offset-1 col-sm-5 text-center balance-element pointer">
                     <div className="amount">{this.props.available_balance}</div>
                     <div className="description">AVAILABLE BALANCE</div>
                 </div>
 
-                <div className="col-sm-5 text-center pointer">
+                <div className="col-sm-5 text-center balance-element pointer">
                     <div className="amount">{this.props.actual_balance}</div>
                     <div className="description">ACTUAL BALANCE</div>
                 </div>
